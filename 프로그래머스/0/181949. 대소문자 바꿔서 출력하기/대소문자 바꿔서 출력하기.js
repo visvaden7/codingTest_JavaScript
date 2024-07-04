571b48778e8c1c0baa8f54a026dca8fc388797e1
visvaden7 <59985708+visvaden7@users.noreply.github.com>
2024-07-04 10:39:14 +0900

    [level 0] Title: 대소문자 바꿔서 출력하기, Time: 43.03 ms, Memory: 32.3 MB -BaekjoonHub

--- 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.js
+++ 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.js
@@ -9,13 +9,13 @@ let input = [];
 rl.on('line', function (line) {
     input = [line];
 }).on('close',function(){
-    str = input[0];
-    let sumstring = ''
-    const sum = [...str].map(it => 
-                 it.charCodeAt() > 96 
-                 ? String.fromCharCode(it.charCodeAt() - 32) 
-                 : String.fromCharCode(it.charCodeAt() + 32))
-    .join('')
-    console.log(sum)
-    
-});
+    let str = input[0];
+    const code = (char) =>  char.charCodeAt()
+    const alphabetDiff = code('a') - code('A')
+    const result = [...str].map(char => {
+        return char.charCodeAt() >= code('a')
+            ? String.fromCharCode(code(char) - alphabetDiff)
+            : String.fromCharCode(code(char) + alphabetDiff)
+    }).join('')
+    console.log(result)
+})
