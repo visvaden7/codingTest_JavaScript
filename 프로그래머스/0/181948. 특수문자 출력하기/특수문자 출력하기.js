84bda5ec242eeb884badbc73c0a03e0387d69d19
visvaden7 <59985708+visvaden7@users.noreply.github.com>
2024-07-04 22:01:32 +0900

    [level 0] Title: 특수문자 출력하기, Time: 48.06 ms, Memory: 32.2 MB -BaekjoonHub

--- 프로그래머스/0/181948. 특수문자 출력하기/특수문자 출력하기.js
+++ 프로그래머스/0/181948. 특수문자 출력하기/특수문자 출력하기.js
@@ -3,7 +3,6 @@ const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
-
 rl.on('close', function () {
-    console.log(`!@#$%^&*(\\\'"<>?:;`)
+    console.log("!@#$%^&*(\\'\"<>?:;")
 });
