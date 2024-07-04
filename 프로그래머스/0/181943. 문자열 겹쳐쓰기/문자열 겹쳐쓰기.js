6b8a5bc7442375a4052496e556af95ae7412f6e5
visvaden7 <59985708+visvaden7@users.noreply.github.com>
2024-07-05 01:14:07 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
@@ -1,8 +1,7 @@
 function solution(my_string, overwrite_string, s) {
-    let temp = my_string.slice(0,s)
-    temp += overwrite_string
-    if(my_string.length - s > overwrite_string.length){
-        temp +=my_string.slice(overwrite_string.length + s, my_string.length)
-    }
-    return temp
+    return my_string
+        .slice(0,s) 
+        + overwrite_string 
+        + my_string
+        .slice(overwrite_string.length + s, my_string.length)
 }
