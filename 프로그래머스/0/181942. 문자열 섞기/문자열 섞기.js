b458112713c5e62b69f45c322e60ab85754d0543
visvaden7 <59985708+visvaden7@users.noreply.github.com>
2024-07-05 16:43:50 +0900

    [level 0] Title: 문자열 섞기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.js
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.js
@@ -1,8 +1,3 @@
 function solution(str1, str2) {
-    let strlength = str1.length
-    let sumString = ''
-    for(let i = 0; i < strlength; i++ ){
-        sumString += str1[i]+str2[i]
-    }
-    return sumString;
+    return [...str1].reduce((result, _, idx) => `${result}${str1[idx]}${str2[idx]}`, "");
 }
