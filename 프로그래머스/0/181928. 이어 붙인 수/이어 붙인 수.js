7687d522d62ce1c1497fc1f57cde89eea83cd9e3
visvaden7 <59985708+visvaden7@users.noreply.github.com>
2024-08-02 02:59:35 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.js
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.js
@@ -1,12 +1,7 @@
 function solution(num_list) {
-    let odd = ""
-    let even = ""
-    num_list.forEach((num) => {
-        if(num % 2) {
-            odd += num
-        }else {
-            even += num
-        }
-    })
+    const {odd, even} = num_list.reduce(({odd, even},num) => {
+        num%2 ? odd += num : even += num
+        return {odd, even}
+    },{odd: "", even: ""})
     return +odd + +even;
 }
