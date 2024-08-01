92e0ee94ce15f7a72d0c029195e12663bb9a2be3
visvaden7 <59985708+visvaden7@users.noreply.github.com>
2024-08-02 02:23:45 +0900

    [level 0] Title: 배열 조각하기, Time: 1.15 ms, Memory: 39 MB -BaekjoonHub

--- 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.js
+++ 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.js
@@ -1,10 +1,18 @@
+
+
+// query를 순회하면서 다음 작업을 반복합니다.
+
+// 짝수 인덱스에서는 arr에서 query[i]번 인덱스를 제외하고 배열의 query[i]번 인덱스 뒷부분을 잘라서 버립니다.
+// 홀수 인덱스에서는 arr에서 query[i]번 인덱스는 제외하고 배열의 query[i]번 인덱스 앞부분을 잘라서 버립니다.
+// 위 작업을 마친 후 남은 arr의 부분 배열을 return 하는 solution 함수를 완성해 주세요.
+
 function solution(arr, query) {
-  query.forEach((query, idx) => {
-    if (idx % 2) {
-      arr.splice(0, query);
-    } else {
-      arr.splice(query + 1, arr.length - query);
-    }
-  });
-  return arr;
+    query.forEach((idx) => {
+        if(idx % 2){
+            arr.splice(0,idx)
+        }else {
+            arr.splice(idx + 1, arr.length - idx)    
+        }
+    })
+    return arr;
 }
