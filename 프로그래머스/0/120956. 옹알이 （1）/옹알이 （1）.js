d5b599750960fac4ada8f521bef1b13488df0b89
visvaden7 <59985708+visvaden7@users.noreply.github.com>
2024-08-08 22:54:14 +0900

    [level 0] Title: 옹알이 (1), Time: 0.43 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.js
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.js
@@ -1,23 +1,13 @@
 function solution(babbling) {
     let answer = 0;
     let babblingCheckList = ["aya","ye","woo","ma"];
-    let tempList = []
-    for(let i = 0; i < babbling.length; i++){
-        let temp = babbling[i]
-        for(let j = 0; j < babblingCheckList.length; j++){
-            if(temp.includes(babblingCheckList[j])){
-                tempList = [...tempList, j]
-            }
-        }
-        console.log(i, tempList)
-        for(let j= 0; j < babblingCheckList.length; j++){
-            temp = temp.replace(babblingCheckList[tempList[j]],"")
-            if(temp.length === 0){
-                answer++
-                break;
-            }
-        }
-        tempList = []
-    }
+    babbling.forEach((oneOfBabbling)=> {
+        babblingCheckList
+        .filter(item => oneOfBabbling.includes(item))
+        .forEach(filteredBabbling => {
+            oneOfBabbling = oneOfBabbling.replace(filteredBabbling,"")
+            oneOfBabbling.length === 0 ? answer++ : answer;
+        })
+    })
     return answer;
 }
